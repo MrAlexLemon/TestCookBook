b04f985a9e4efd3834e605f1bed7c9aa7c41fa60
Alex <lileynuk@gmail.com>
2021-03-01 19:22:55 +0200

    Added relationships between projects.

---
+++ CookBook/CookBook/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CookBook.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok("Test.");
+        }
+    }
+}
